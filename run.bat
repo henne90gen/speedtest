ba877f2d867d9b53a6d639b81764ee222561d38b
Hendrik Müller <hendrik.mueller@devboost.de>
2018-02-27 17:28:26 +0100

    Fixing run script and removing run.bat

--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-python D:\henne\Workspace\Python\speedtest\custom_speedtest.py %*
-pause
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python custom_speedtest.py >> speed.log 2>&1
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR && python custom_speedtest.py >> speed.log 2>&1
